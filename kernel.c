d188051fa09a4761c44bae84cbfa4b77708dec01
rayanmargham <69608259+rayanmargham@users.noreply.github.com>
2022-05-18 20:57:37 +0100

    GDT lol

--- kernel.c
+++ kernel.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <limine.h>
+#include <src/gdt.h>
  
 // The Limine requests can be placed anywhere, but it is important that
 // the compiler does not optimise them away, so, usually, they should
@@ -10,12 +11,17 @@ static volatile struct limine_terminal_request terminal_request = {
     .id = LIMINE_TERMINAL_REQUEST,
     .revision = 0
 };
+struct limine_bootloader_info_request bootloader_info_request = {
+    .id = LIMINE_BOOTLOADER_INFO_REQUEST,
+    .revision = 0
+};
 
 static void done(void) {
     for (;;) {
         __asm__("hlt");
     }
 }
+
 
 // The following will be our kernel's entry point.
 void _start(void) {
@@ -28,10 +34,22 @@ void _start(void) {
     // We should now be able to call the Limine terminal to print out
     // a simple "Hello World" to screen.
     struct limine_terminal *terminal = terminal_request.response->terminals[0];
-    terminal_request.response->write(terminal, "PHOTON KERNEL IS \e[0;32mLOADING\e[0m \n", sizeof("PHOTON KERNEL IS \e[0;32mLOADING\e[0m \n") - 1);
-    terminal_request.response->write(terminal, "Please Standby... \n", sizeof("Please Standby... \n") - 1);
-    
- 
+    terminal_request.response->write(terminal, "PHOTON KERNEL IS \e[0;32mLOADING\e[0m \n", sizeof("PHOTON KERNEL IS \e[0;32mLOADING\e[0m \n"));
+    terminal_request.response->write(terminal, "Please Standby... \n", sizeof("Please Standby... \n"));
+    struct limine_bootloader_info_response *bootloader_info = bootloader_info_request.response;
+    // print out limines version and name in green
+    terminal_request.response->write(terminal, "[Kernel] BOOTLOADER: ", sizeof("[Kernel] BOOTLOADER: "));
+    terminal_request.response->write(terminal, "\e[0;32m", sizeof("\e[0;32m"));
+    terminal_request.response->write(terminal, bootloader_info->name, sizeof(bootloader_info->name));
+    terminal_request.response->write(terminal, " V ", sizeof(" V "));
+    terminal_request.response->write(terminal, bootloader_info->version, sizeof(bootloader_info->version));
+    terminal_request.response->write(terminal, "\e[0m\n", sizeof("\e[0m\n"));
+    uint64_t gdt = init_gdt();
+    terminal_request.response->write(terminal, "[Kernel] GDT Loaded\n", sizeof("[Kernel] GDT Loaded\n"));
+    terminal_request.response->write(terminal, "[Kernel] GDT Entries: ", sizeof("[Kernel] GDT Entries: "));
+
+    //terminal_request.response->write(terminal, "[Kernel] GDT: ", sizeof("[Kernel] GDT: ") - 1);
     // We're done, just hang...
     done();
+    
 }
---
+++ src/gdt.c
@@ -0,0 +1,15 @@
+#include "../limine.h"
+static uint64_t gdt[] = {0x0000000000000000, 0x00009a000000ffff, 0x000093000000ffff, 0x00cf9a000000ffff, 0x00cf93000000ffff, 0x00af9b000000ffff, 0x00af93000000ffff, 0x00affb000000ffff, 0x00aff3000000ffff};
+
+struct gdtr_t {
+    uint16_t limit;
+    uint64_t base;
+} __attribute__((packed));
+// This function will initialize the GDT and return the GDT Entries.
+uint64_t init_gdt(void) 
+{
+    
+    struct gdtr_t gdtr = {0x48, (uint64_t)gdt};
+    __asm__ volatile ("lgdt %0" :: "m" (gdtr));
+    return gdt;
+};
